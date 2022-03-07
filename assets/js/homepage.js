6c2be8ebcb363364b23b20bd1fa180011952d098
Claudia Chittim <cchittim10101@gmail.com>
2022-03-07 17:26:04 -0600

    added facebook to user repo

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,21 @@
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function ("facebook") {
+  //format the github api url
+  var apiUrl = "https://api.github.com/facebook/" + facebook + "/repos";
+
+  //make a request to the URL
+  fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+      console.log(data);
+  });
+});
 };
 
-getUserRepos();
+ // fetch("https://api.github.com/users/octocat/repos");
+};
+
+//getUserRepos(user); // https://api.github.com/users/<user>/repos);
+//var response = fetch("https://api.github.com/users/octocat/repos").then(function(response)response.json().then(function(data) {
+//console.log(data)
+});
+
+});
